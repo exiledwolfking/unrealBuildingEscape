21dddca56ed8b59a03360ad15042b669378ea1c7
exiledwolfking <kylebwahl@gmail.com>
2018-11-17 18:31:18 -0600

    Added Grabber and GameMode_BP and Pawn_BP

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,49 @@
+// Copyright Kyle Wahl 2018
+
+#include "Grabber.h"
+//#include "Engine/World.h"
+//#include "Math/Vector.h"
+//#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	//Owner = GetOwner();
+	//UWorld* world = GetWorld();
+
+	//PlayerController = world->GetFirstPlayerController();
+
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty."));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get player view point this tick
+	//FVector location;
+	//FRotator rotation;
+	//playerController->GetPlayerViewPoint(location, rotation);
+	//UE_LOG(LogTemp, Warning, TEXT("%s-> %s --- %s"), *(Owner->GetName()), *(location.ToString(), *(rotation.ToString())));
+
+	// Ray-cast out to reach distance
+
+	// See what we hit
+}
+
